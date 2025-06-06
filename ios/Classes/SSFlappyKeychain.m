a59969076fd31d0a0ad1a8ebd68a116638d54abe
lilin <327603258@qq.com>
2025-06-06 21:36:53 +0800

    set synchroized false

--- ios/Classes/SSFlappyKeychain.m
+++ ios/Classes/SSFlappyKeychain.m
@@ -18,7 +18,7 @@ NSString *const FlappykSSKeychainLabelKey = @"labl";
 NSString *const FlappykSSKeychainLastModifiedKey = @"mdat";
 NSString *const FlappykSSKeychainWhereKey = @"svce";
 
-#if __IPHONE_4_0 && TARGET_OS_IPHONE  
+#if __IPHONE_4_0 && TARGET_OS_IPHONE
 CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 #endif
 
@@ -49,26 +49,26 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
     OSStatus status = FlappySSKeychainErrorBadArguments;
     NSMutableDictionary *query = [self _queryForService:service account:nil];
 #if __has_feature(objc_arc)
-	[query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnAttributes];
+    [query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnAttributes];
     [query setObject:(__bridge id)kSecMatchLimitAll forKey:(__bridge id)kSecMatchLimit];
 #else
     [query setObject:(id)kCFBooleanTrue forKey:(id)kSecReturnAttributes];
     [query setObject:(id)kSecMatchLimitAll forKey:(id)kSecMatchLimit];
 #endif
-	
-	CFTypeRef result = NULL;
+    
+    CFTypeRef result = NULL;
 #if __has_feature(objc_arc)
     status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
 #else
-	status = SecItemCopyMatching((CFDictionaryRef)query, &result);
+    status = SecItemCopyMatching((CFDictionaryRef)query, &result);
 #endif
     if (status != noErr && error != NULL) {
-		*error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
-		return nil;
-	}
-	
+        *error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
+        return nil;
+    }
+    
 #if __has_feature(objc_arc)
-	return (__bridge_transfer NSArray *)result;
+    return (__bridge_transfer NSArray *)result;
 #else
     return [(NSArray *)result autorelease];
 #endif
@@ -78,21 +78,21 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 #pragma mark - Getting Passwords
 
 + (NSString *)passwordForService:(NSString *)service account:(NSString *)account {
-	return [self passwordForService:service account:account error:nil];
+    return [self passwordForService:service account:account error:nil];
 }
 
 
 + (NSString *)passwordForService:(NSString *)service account:(NSString *)account error:(NSError **)error {
     NSData *data = [self passwordDataForService:service account:account error:error];
-	if (data.length > 0) {
-		NSString *string = [[NSString alloc] initWithData:(NSData *)data encoding:NSUTF8StringEncoding];
+    if (data.length > 0) {
+        NSString *string = [[NSString alloc] initWithData:(NSData *)data encoding:NSUTF8StringEncoding];
 #if !__has_feature(objc_arc)
-		[string autorelease];
+        [string autorelease];
 #endif
-		return string;
-	}
-	
-	return nil;
+        return string;
+    }
+    
+    return nil;
 }
 
 
@@ -103,32 +103,32 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 
 + (NSData *)passwordDataForService:(NSString *)service account:(NSString *)account error:(NSError **)error {
     OSStatus status = FlappySSKeychainErrorBadArguments;
-	if (!service || !account) {
-		if (error) {
-			*error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
-		}
-		return nil;
-	}
-	
-	CFTypeRef result = NULL;	
-	NSMutableDictionary *query = [self _queryForService:service account:account];
+    if (!service || !account) {
+        if (error) {
+            *error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
+        }
+        return nil;
+    }
+    
+    CFTypeRef result = NULL;
+    NSMutableDictionary *query = [self _queryForService:service account:account];
 #if __has_feature(objc_arc)
-	[query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
-	[query setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
-	status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
+    [query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    [query setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
 #else
-	[query setObject:(id)kCFBooleanTrue forKey:(id)kSecReturnData];
-	[query setObject:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
-	status = SecItemCopyMatching((CFDictionaryRef)query, &result);
+    [query setObject:(id)kCFBooleanTrue forKey:(id)kSecReturnData];
+    [query setObject:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
+    status = SecItemCopyMatching((CFDictionaryRef)query, &result);
 #endif
-	
-	if (status != noErr && error != NULL) {
-		*error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
-		return nil;
-	}
-	
+    
+    if (status != noErr && error != NULL) {
+        *error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
+        return nil;
+    }
+    
 #if __has_feature(objc_arc)
-	return (__bridge_transfer NSData *)result;
+    return (__bridge_transfer NSData *)result;
 #else
     return [(NSData *)result autorelease];
 #endif
@@ -138,24 +138,24 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 #pragma mark - Deleting Passwords
 
 + (BOOL)deletePasswordForService:(NSString *)service account:(NSString *)account {
-	return [self deletePasswordForService:service account:account error:nil];
+    return [self deletePasswordForService:service account:account error:nil];
 }
 
 
 + (BOOL)deletePasswordForService:(NSString *)service account:(NSString *)account error:(NSError **)error {
-	OSStatus status = FlappySSKeychainErrorBadArguments;
-	if (service && account) {
-		NSMutableDictionary *query = [self _queryForService:service account:account];
+    OSStatus status = FlappySSKeychainErrorBadArguments;
+    if (service && account) {
+        NSMutableDictionary *query = [self _queryForService:service account:account];
 #if __has_feature(objc_arc)
-		status = SecItemDelete((__bridge CFDictionaryRef)query);
+        status = SecItemDelete((__bridge CFDictionaryRef)query);
 #else
-		status = SecItemDelete((CFDictionaryRef)query);
+        status = SecItemDelete((CFDictionaryRef)query);
 #endif
-	}
-	if (status != noErr && error != NULL) {
-		*error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
-	}
-	return (status == noErr);
+    }
+    if (status != noErr && error != NULL) {
+        *error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
+    }
+    return (status == noErr);
     
 }
 
@@ -163,7 +163,7 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 #pragma mark - Setting Passwords
 
 + (BOOL)setPassword:(NSString *)password forService:(NSString *)service account:(NSString *)account {
-	return [self setPassword:password forService:service account:account error:nil];
+    return [self setPassword:password forService:service account:account error:nil];
 }
 
 
@@ -180,52 +180,56 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 
 + (BOOL)setPasswordData:(NSData *)password forService:(NSString *)service account:(NSString *)account error:(NSError **)error {
     OSStatus status = FlappySSKeychainErrorBadArguments;
-	if (password && service && account) {
+    if (password && service && account) {
         [self deletePasswordForService:service account:account];
         NSMutableDictionary *query = [self _queryForService:service account:account];
 #if __has_feature(objc_arc)
-		[query setObject:password forKey:(__bridge id)kSecValueData];
+        [query setObject:password forKey:(__bridge id)kSecValueData];
 #else
-		[query setObject:password forKey:(id)kSecValueData];
+        [query setObject:password forKey:(id)kSecValueData];
 #endif
-		
+        
+        //设置同步属性为 false，确保数据仅存储在本地设备上
+        [query setObject:(__bridge id)kCFBooleanFalse forKey:(__bridge id)kSecAttrSynchronizable];
+        
+        
 #if __IPHONE_4_0 && TARGET_OS_IPHONE
-		if (FlappySSKeychainAccessibilityType) {
+        if (FlappySSKeychainAccessibilityType) {
 #if __has_feature(objc_arc)
-			[query setObject:(id)[self accessibilityType] forKey:(__bridge id)kSecAttrAccessible];
+            [query setObject:(id)[self accessibilityType] forKey:(__bridge id)kSecAttrAccessible];
 #else
-			[query setObject:(id)[self accessibilityType] forKey:(id)kSecAttrAccessible];
+            [query setObject:(id)[self accessibilityType] forKey:(id)kSecAttrAccessible];
 #endif
-		}
+        }
 #endif
-		
+        
 #if __has_feature(objc_arc)
         status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
 #else
-		status = SecItemAdd((CFDictionaryRef)query, NULL);
+        status = SecItemAdd((CFDictionaryRef)query, NULL);
 #endif
-	}
-	if (status != noErr && error != NULL) {
-		*error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
-	}
-	return (status == noErr);
+    }
+    if (status != noErr && error != NULL) {
+        *error = [NSError errorWithDomain:FlappykSSKeychainErrorDomain code:status userInfo:nil];
+    }
+    return (status == noErr);
 }
 
 
 #pragma mark - Configuration
 
-#if __IPHONE_4_0 && TARGET_OS_IPHONE 
+#if __IPHONE_4_0 && TARGET_OS_IPHONE
 + (CFTypeRef)accessibilityType {
-	return FlappySSKeychainAccessibilityType;
+    return FlappySSKeychainAccessibilityType;
 }
 
 
 + (void)setAccessibilityType:(CFTypeRef)accessibilityType {
-	CFRetain(accessibilityType);
-	if (FlappySSKeychainAccessibilityType) {
-		CFRelease(FlappySSKeychainAccessibilityType);
-	}
-	FlappySSKeychainAccessibilityType = accessibilityType;
+    CFRetain(accessibilityType);
+    if (FlappySSKeychainAccessibilityType) {
+        CFRelease(FlappySSKeychainAccessibilityType);
+    }
+    FlappySSKeychainAccessibilityType = accessibilityType;
 }
 #endif
 
@@ -237,25 +241,25 @@ CFTypeRef FlappySSKeychainAccessibilityType = NULL;
 #if __has_feature(objc_arc)
     [dictionary setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
 #else
-	[dictionary setObject:(id)kSecClassGenericPassword forKey:(id)kSecClass];
+    [dictionary setObject:(id)kSecClassGenericPassword forKey:(id)kSecClass];
 #endif
-	
+    
     if (service) {
 #if __has_feature(objc_arc)
-		[dictionary setObject:service forKey:(__bridge id)kSecAttrService];
+        [dictionary setObject:service forKey:(__bridge id)kSecAttrService];
 #else
-		[dictionary setObject:service forKey:(id)kSecAttrService];
+        [dictionary setObject:service forKey:(id)kSecAttrService];
 #endif
-	}
-	
+    }
+    
     if (account) {
 #if __has_feature(objc_arc)
-		[dictionary setObject:account forKey:(__bridge id)kSecAttrAccount];
+        [dictionary setObject:account forKey:(__bridge id)kSecAttrAccount];
 #else
-		[dictionary setObject:account forKey:(id)kSecAttrAccount];
+        [dictionary setObject:account forKey:(id)kSecAttrAccount];
 #endif
-	}
-	
+    }
+    
     return dictionary;
 }
 
